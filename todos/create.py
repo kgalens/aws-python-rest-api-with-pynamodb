dd91c038c02806ac3849d812d3662c5a783d4414
Kevin Galens <kggalens@amazon.com>
2021-06-25 11:23:23 -0400

    Change

--- todos/create.py
+++ todos/create.py
@@ -6,7 +6,7 @@ from todos.todo_model import TodoModel
 
 
 def create(event, context):
-    """ Adding less documentation """
+    """ Adding more again documentation """
     data = json.loads(event['body'])
     if 'text' not in data:
         logging.error('Validation Failed')
